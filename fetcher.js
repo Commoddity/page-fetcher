526f85c7c9c1bbe8c2f5bf7a914b0d5bb487c585
Pascal <pascalvanleeuwen604@gmail.com>
2019-12-05 20:51:46 +0000

    Changed file to run asynchronously.

--- fetcher.js
+++ fetcher.js
@@ -5,36 +5,40 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+
 });
 
 const pageFetcher = (url, dest) => {
   request(url, (error, response, body) => {
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
-    if (!fs.existsSync(dest)) {
-      fs.writeFile(dest, body, (err) => {
-        if (err) throw err;
-        console.log(`Downloaded and saved to ${dest}`);
-      });
-    }
-    if (fs.existsSync(dest)) {
-      rl.question(`${dest} already exists. Overwrite file? [Y/N] `, (answer) => {
+    // console.log('body:', body);
+    fs.access(dest, fs.F_OK, (err) => {
+      if (err) {
+        fs.writeFile(dest, body, (err) => {
+          if (err) throw err;
+          console.log(`Downloaded and saved to ${dest}`);
+        });
         rl.close();
-        if (answer === 'y' || answer.toLowerCase() === 'y') {
-          fs.writeFile(dest, body, (err) => {
-            if (err) throw err;
-            console.log(`Downloaded and saved to ${dest}`);
-          });
-        } else {
-          console.log("Invalid input. Input must be [Y/N]");
-        }
-      });
-    } else {
-      rl.close();
-    }
+      } else if (!err) {
+        rl.question(`${dest} already exists. Overwrite file? [Y/N] `, (answer) => {
+          rl.close();
+          if (answer === 'y' || answer.toLowerCase() === 'y') {
+            fs.writeFile(dest, body, (err) => {
+              if (err) throw err;
+              console.log(`Downloaded and saved to ${dest}`);
+            });
+          } else {
+            console.log("Invalid input. Input must be [Y/N]");
+          }
+        });
+      } else {
+        rl.close();
+      }
+    });
   });
 };
+
 
 rl.question("Enter page URL to download: ", (pageUrl) => {
   rl.question("Enter destination file path: ", (destPath) => {
