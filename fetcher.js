0ff9ebfef608df576aa921a5cfe52b121fd4989f
Pascal <pascalvanleeuwen604@gmail.com>
2019-12-05 04:11:51 +0000

    Finished assignment and strech exercise.

---
+++ fetcher.js
@@ -0,0 +1,37 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const pageFetcher = (url, dest) => {
+  request(url, (error, response, body) => {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+    if (!fs.existsSync(dest)) {
+      fs.writeFile(dest, body, (err) => {
+        if (err) throw err;
+        console.log(`Downloaded and saved to ${dest}`);
+      });
+    }
+    if (fs.existsSync(dest)) {
+      rl.question(`${dest} already exists. Overwrite file? [Y/N] `, (answer) => {
+        rl.close();
+        if (answer === 'y' || answer.toLowerCase() === 'y') {
+          fs.writeFile(dest, body, (err) => {
+            if (err) throw err;
+            console.log(`Downloaded and saved to ${dest}`);
+          });
+        } else {
+          console.log("Invalid input. Input must be [Y/N]");
+        }
+      });
+    }
+  });
+};
+
+pageFetcher("http://www.example.com", "./index.html");
